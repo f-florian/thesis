d7777d7ae129b4d0708ca649dd3974c6b7921f9a
Francesco Florian <francesco.florian94@gmail.com>
2018-02-03 15:18:39 +0100

    EE ok

--- resolv/eigen.cpp
+++ resolv/eigen.cpp
@@ -1,6 +1,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_eigen.h>
 #include <gsl/gsl_complex_math.h>
+// #include <gsl/gsl_blas.h>
 
 #include "eigen.h"
 #include "interpolation.h"
@@ -10,7 +11,6 @@ using namespace interpolation;
 namespace eigen {
     namespace {
         gsl_matrix *A,*F;
-        gsl_eigen_gen_workspace *ws;
         gsl_vector_complex * alpha;
         gsl_vector * beta;
     }
@@ -24,19 +24,16 @@ namespace eigen {
             gsl_vector_free(beta);
         if(alpha!=NULL)
             gsl_vector_complex_free(alpha);
-        gsl_eigen_gen_free(ws);
-
     }
-    void init(const unsigned int size, double start, double delta)
-    {
-        freemem();
-        auto invd=1/delta;
-        //todo: A sparse?
-        //todo2: A already triangular!
-        A=gsl_matrix_calloc(size,size);
-        F=gsl_matrix_calloc(size,size);
-        ws=gsl_eigen_gen_alloc(size);
-        
+	void init(const unsigned int size, double start, double delta)
+	{
+		freemem();
+		auto invd=1/delta;
+		//todo: A sparse?
+		//todo2: A already triangular!
+		A=gsl_matrix_calloc(size,size);
+		F=gsl_matrix_calloc(size,size);
+		
         for(int i=0;i<size-1; i++) {
             auto curnode=start+delta*i;
             gsl_matrix_set(A,i,i,-invd-gamma(curnode)-mu(curnode));
@@ -52,24 +49,34 @@ namespace eigen {
         for(int j=0; j<size; j++){
             gsl_matrix_set(F,size-1,j,dasi*interpolation::beta(curnode,start+delta*j));
         }
+        beta=gsl_vector_alloc(size);
+        alpha=gsl_vector_complex_alloc(size);
     }
     double computeSpectralRadius()
     {
-        // printf("init\n");
-        // fflush(stdout);
-        beta=gsl_vector_alloc(A->size1);
-        alpha=gsl_vector_complex_alloc(A->size1);
+	    double a,radius=0;
+	    // general eigenvalue problem
+        gsl_eigen_gen_workspace *ws=gsl_eigen_gen_alloc(A->size1);
         gsl_eigen_gen(F,A,alpha,beta,ws);
-        // printf("calc\n");
-        // fflush(stdout);
-        double radius=0;
         for(int i=0; i<A->size1; i++){
-            double a=gsl_complex_abs(gsl_vector_complex_get(alpha,i))/abs(gsl_vector_get(beta,i));
+            a=gsl_complex_abs(gsl_vector_complex_get(alpha,i))/abs(gsl_vector_get(beta,i));
             if(radius<a)
                 radius=a;
             // printf("eigenvalue %d=%lf\n", i,a);
             // fflush(stdout);
         }
+        gsl_eigen_gen_free(ws);
+
+        // // standard eigenvalue problem
+        // gsl_blas_dtrsm(CblasRight,  CblasLower,  CblasNoTrans, CblasNonUnit, 1, A, F);
+        // gsl_eigen_nonsymm_workspace *ws2= gsl_eigen_nonsymm_alloc(A->size1);
+        // gsl_eigen_nonsymm(F, alpha, ws2);
+        // for(int i=0; i<A->size1; i++){
+        //     a=gsl_complex_abs(gsl_vector_complex_get(alpha,i));
+        //     if(radius<a)
+        //         radius=a;
+        // }
+        // gsl_eigen_nonsymm_free(ws2);
         return radius;
     }
 }
--- resolv/interpolation.cpp
+++ resolv/interpolation.cpp
@@ -11,31 +11,25 @@ namespace interpolation
         gsl_spline *splinemu;
         gsl_spline *splineS0;
     }
-    const double k=0.00003;
+    const double k=3e-5;
     double beta(double x1, double x2){
-        return k*0.6*(10000-(x1-x2)*(x1-x2))*0.000001+0.001;
+	    return k*(6e-7*(10000-(x1-x2)*(x1-x2))+0.001);
     }
     double gamma(double x){
         return 52;
     }
     double S0(double x)
     {
-	    if((x-100)<0.00001)
-		    return 65;
-        printf("s(%lf)", x);
-        fflush(stdout);
+        // if((x-100.)<0.00001){
+	//         printf("anoeinaos\n");
+	//         return 65;
+        // }
         auto tmp=gsl_spline_eval(splineS0,x,accels);
-        printf("=%lf\n", tmp);
-        fflush(stdout);
         return tmp;
     }
     double mu(double x)
     {
-        printf("mu(%lf)", x);
-        fflush(stdout);
         auto tmp=gsl_spline_eval(splinemu,x,accelm);
-        printf("=%lf\n", tmp);
-        fflush(stdout);
         return tmp;
     }
     void splineInit(int sizes, int sizem, const double mx[], const double my[], const double sx[], const double sy[])
@@ -47,8 +41,8 @@ namespace interpolation
 	accels=gsl_interp_accel_alloc();
 	accelm=gsl_interp_accel_alloc();
 	splinemu=gsl_spline_alloc(gsl_interp_cspline, sizem);
-        splineS0=gsl_spline_alloc(gsl_interp_cspline, sizes);
-        gsl_spline_init(splinemu, mx, my, sizem);
-        gsl_spline_init(splineS0, sx, sy, sizes);
+	splineS0=gsl_spline_alloc(gsl_interp_cspline, sizes);
+	gsl_spline_init(splinemu, mx, my, sizem);
+	gsl_spline_init(splineS0, sx, sy, sizes);
     }
 }
