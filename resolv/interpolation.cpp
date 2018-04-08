6b4bc2410165e27519b053d5a868708112f509a1
Francesco Florian <francesco.florian94@gmail.com>
2018-04-08 22:31:21 +0200

    analitic

--- resolv/interpolation.cpp
+++ resolv/interpolation.cpp
@@ -13,38 +13,48 @@ namespace interpolation
     }
     const double k=3e-5;
     double beta(const double x1, const double x2){
-	    return k*(6e-7*(10000-(x1-x2)*(x1-x2))+0.001);
+        return k*(6e-7*(10000-(x1-x2)*(x1-x2))+0.001);
     }
     double gamma(const double x){
         return 52;
     }
     double S0(const double x)
     {
-        if(x>100){
-            printf("Requested interpoation in %.2e, which is %.2e beyond the maximum value\n",x,x-100);
-	        return gsl_spline_eval(splineS0,100,accels);;
-        }
-        return gsl_spline_eval(splineS0,x,accels);
+        return 5187+ x*(226.438-2.776583333333333-x*2.776583333333333);
+        // if(x>100){
+        //     fprintf(stderr,"Requested s interpolation in %.2e, which is %.2e beyond the maximum value\n",x,x-100);
+        //     return gsl_spline_eval(splineS0,100,accels);;
+        // }
+        // if(x<0){
+        //     fprintf(stderr,"Requested s interpolation in %.2e, which is %.2e below the minimum value\n",x,x);
+        //     return gsl_spline_eval(splineS0,100,accels);;
+        // }
+        // return gsl_spline_eval(splineS0,x,accels);
     }
     double mu(const double x)
     {
-        if(x>100){
-            printf("Requested interpoation in %.2e, which is %.2e beyond the maximum value\n",x,x-100);
-            return gsl_spline_eval(splinemu,100,accelm);;
-        }
-        return gsl_spline_eval(splinemu,x,accelm);
+        return 8.3675/(101-x);
+        // if(x>100){
+        //     fprintf(stderr,"Requested interpolation in %.2e, which is %.2e beyond the maximum value\n",x,x-100);
+        //     return gsl_spline_eval(splinemu,100,accelm);;
+        // }
+        // if(x<0){
+        //     fprintf(stderr,"Requested mu interpolation in %.2e, which is %.2e below the minimum value\n",x,x);
+        //     return gsl_spline_eval(splineS0,100,accels);
+        // }
+        // return gsl_spline_eval(splinemu,x,accelm);
     }
-    void splineInit(const int sizes, const int sizem, const double mx[], const double my[], const double sx[], const double sy[])
+    void splineInit(const int sizes, const int sizem, const double *mx, const double *my, const double sx[], const double sy[])
     {
-	if(accels!=NULL)
-	    gsl_interp_accel_free(accels);
-	if(accelm!=NULL)
-	    gsl_interp_accel_free(accelm);
-	accels=gsl_interp_accel_alloc();
-	accelm=gsl_interp_accel_alloc();
-	splinemu=gsl_spline_alloc(gsl_interp_cspline, sizem);
-	splineS0=gsl_spline_alloc(gsl_interp_cspline, sizes);
-	gsl_spline_init(splinemu, mx, my, sizem);
-	gsl_spline_init(splineS0, sx, sy, sizes);
+        if(accels!=NULL)
+            gsl_interp_accel_free(accels);
+        if(accelm!=NULL)
+            gsl_interp_accel_free(accelm);
+        accels=gsl_interp_accel_alloc();
+        accelm=gsl_interp_accel_alloc();
+        splinemu=gsl_spline_alloc(gsl_interp_cspline, sizem);
+        splineS0=gsl_spline_alloc(gsl_interp_cspline, sizes);
+        gsl_spline_init(splinemu, mx, my, sizem);
+        gsl_spline_init(splineS0, sx, sy, sizes);
     }
 }
