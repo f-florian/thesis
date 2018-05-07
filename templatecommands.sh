11bcb1a193b5b32aaa674a30c000f04bb46c51ac
Francesco Florian <francesco.florian94@gmail.com>
2018-05-07 09:30:27 +0200

    tracking bash 'for' command

---
+++ templatecommands.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+refa=2.59058965581388835064e-03
+refs=2.28581340977200442310e+00
+# ref=$refa; max=1000; for i in 1 2 3 4 5 6 7 8; do ((fname="../../../5/out/data/a"$max"g.rk"$i; resolv/resolv 1 $i 1 1 50 0 $ref > $fname; resolv/resolv 1 $i 100 100 $max 0 $ref >> $fname)&); done
