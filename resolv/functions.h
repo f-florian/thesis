c870a32fb0d004aba17c9e4aabefe9eb4406e0fb
Francesco Florian <francesco.florian94@gmail.com>
2018-05-13 12:59:43 +0200

    copyright notice

--- resolv/functions.h
+++ resolv/functions.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+ * Copyright (C) 2018 Francesco Florian
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 namespace parameters{
     void init(const double length_p, const double c0_p, const double D0_p, const double beta0_p, const double mu0_p, const double c1_p, const double D1_p, const int p_p);
     double c(const double a);
